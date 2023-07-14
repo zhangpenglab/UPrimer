e509b4c4aa87719b6549bc1842727eaf4df6a482
zhangpenglab <139540726+zhangpenglab@users.noreply.github.com>
2023-07-14 22:10:25 +0800

    Add files via upload

---
+++ Accessory/Extract_exome_from_genbank_file.py
@@ -0,0 +1,69 @@
+import os, glob
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+def extractexon(genome):
+        startList = []
+        endList = []
+        descList = []
+	for chromosome in SeqIO.parse(genome, "genbank"):
+		for feature in chromosome.features:
+			if feature.type == "CDS":
+				exonLocList = feature.location.parts
+				try:
+					geneID = feature.qualifiers["db_xref"][0].replace(":", "")
+				except KeyError:
+					geneID = 'unknown'
+				try:
+					proteinID = feature.qualifiers["protein_id"][0]
+				except KeyError:
+					proteinID = ""
+				if not proteinID:
+					proteinID = "Prot_"+geneID
+				product = feature.qualifiers["product"][0]
+				if feature.qualifiers.has_key("gene"):
+					gene = feature.qualifiers["gene"][0]
+				else:
+					gene = geneID
+				for exonLoc in exonLocList:
+					exonSeq = chromosome.seq[exonLoc.start:exonLoc.end]
+					exonStrand = exonLoc.strand
+					if exonStrand > 0:
+						cdsSeq = exonSeq
+					else:
+						cdsSeq = exonSeq.reverse_complement()
+					startList.append(int(exonLoc.start))
+					endList.append(int(exonLoc.end))
+					cdsRecord = SeqRecord(cdsSeq)
+					descList = [proteinID, geneID, gene, chromosome.id, str(exonLoc.start), str(exonLoc.end), str(exonStrand), product]
+					cdsRecord.description = "_".join(descList)
+					yield cdsRecord
+
+
+
+def extractExonInBatch(inDir, outDir):
+	if not os.path.isdir(outDir):
+		os.makedirs(outDir)
+	genomeList = glob.glob(os.path.join(inDir, "*.gbff"))
+	for genome in genomeList:
+                seq_list = []
+		outBase = os.path.basename(genome).replace('_genomic.gbff', '_exon.fasta')
+		print outBase
+
+		outname = os.path.join(outDir, outBase)
+		outFile = open(outname,'w')
+                for exon in extractexon(genome):
+                        seq = exon.seq
+                        description = exon.description
+                        outFile.write('>' + str(description) + '\n' + str(seq) + '\n')
+                outFile.close()
+                        
+                      
+
+if __name__ == '__main__':
+	
+	inDir = os.path.join(os.getcwd(), 'gbff')
+	outDir = os.path.join(os.getcwd(), 'exon')
+	extractExonInBatch(inDir, outDir)
+	
